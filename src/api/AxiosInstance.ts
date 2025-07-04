36a46723f3dd671f1998aafc2bb25d894eabcd8a
Emmanuel Ibenwankwo <EIbenwankwo@providusbank.com>
2025-07-04 12:56:51 +0100

    update

--- src/api/AxiosInstance.ts
+++ src/api/AxiosInstance.ts
@@ -4,7 +4,7 @@ import {getCookie} from "@/utils/CookieManagement";
 
 // Create Axios instance
 const axiosInstance = axios.create({
-    baseURL: "https://propellant-latest.onrender.com/api/v1",
+    baseURL: "/api/v1",
     headers: {
         'Content-Type': 'application/json',
     },
